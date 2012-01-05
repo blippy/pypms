bd0b47932d03705ce71910999468efb1b7377c6f
mark carter <mcturra2000@yahoo.co.uk>
2012-01-05 11:28:07 +0000

    linted rtfsprint.py

--- rtfsprint.py
+++ rtfsprint.py
@@ -1,18 +1,16 @@
-import datetime
+"""Print the RTF files"""
+
 import glob
 import os
-
-import win32api, win32com.client
 
 import common
 import db
 import period
-from common import princ
 
 def print_file(filename):
+    "Print a file using wordpad"
     cmd = '"C:\Program Files\Windows NT\Accessories\wordpad.exe" /p ' + filename
     os.system(cmd)
-    #win32api.ShellExecute(0, "print", fname, None, ".", 0)
 
 def work_statements():
     'Print workstatements'
@@ -20,21 +18,28 @@ def work_statements():
     files = glob.glob(pattern)
     files.sort()
     for fname in files:
-        f0 = os.path.basename(fname)[0]
-        if f0 in ['0','5']: continue # special files that don't need to be printed
+        
+        # bail out on special files
+        fname0 = os.path.basename(fname)[0]
+        if fname0 in ['0','5']: 
+            continue
+        
         print_file(fname)
     
 def timesheets(debug = False):
+    "Print timesheets"
     jobs = [x['job'] for x in db.GetJobs().values() if x['Autoprint']]
     jobs.sort()
     for job in jobs:
         fname = '{0}\\timesheets\\{1}.rtf'.format(period.perioddir(), job)
-        if not os.path.isfile(fname): continue
-        if debug: print fname
+        if not os.path.isfile(fname):
+            continue
+        if debug:
+            print fname
         else: print_file(fname)
 
     
 if  __name__ == "__main__":
+    work_statements()
     timesheets(debug = True)
-    #work_statements()
-    princ("Finished xls")
+    common.princ("Finished xls")
